722c4bd4ecceb757f7539299fe8beda6f381366a
martin kaboja <martinkaboja@gmail.com>
2023-10-08 19:53:05 +0300

    .

--- routes/api.php
+++ routes/api.php
@@ -603,7 +603,7 @@ Route::get('/bangUpdateComment/{id}', function($id){
             'user' => function($query) {
                 $query->select('id', 'name', 'image');
             },
-        ])->orderBy('created_at', 'desc')->get();
+        ])->orderBy('created_at', 'asc')->get();
     return response()->json(['comments' => $comments]);
 });
 
