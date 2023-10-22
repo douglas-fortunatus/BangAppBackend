122423e2c6835eef1836551647248ff1bf2ea2d5
hermesgido <hermessgido@gmail.com>
2023-10-22 16:38:48 +0300

    s

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -128,7 +128,7 @@ class ChatController extends Controller
         'message' => $messageText,
         'message_type' => $messageType ?? 'text',
     ]);
-    $receiver = User::find($user2_id);
+    $receiver = User::find($sender_id);
     $conversation->messages()->save($message);
     $pushNotificationService = new PushNotificationService();
     $pushNotificationService->sendPushNotification($receiver->device_token, $receiver->name, "New Message:" .$messageText, $conversation->id);
