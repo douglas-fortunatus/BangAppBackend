f982904246b37d03d44c5698253955176a774e71
martin kaboja <martinkaboja@gmail.com>
2023-10-22 17:04:22 +0300

    change and added notification type

--- app/Http/Controllers/PushNotificationService.php
+++ app/Http/Controllers/PushNotificationService.php
@@ -12,13 +12,13 @@ use Kreait\Firebase\Messaging\CloudMessage;
 class PushNotificationService extends Controller
 {
     //
-    public function sendPushNotification($deviceToken, $title, $body,$challengeId)
+    public function sendPushNotification($deviceToken,$title, $body,$notificationId,$type)
     {
         $messaging = app('firebase.messaging');
         // Create a new Message instance with the notification details
         $message = CloudMessage::withTarget('token', $deviceToken)
             ->withNotification(Notification::create($title, $body))
-            ->withData(['challenge_id' => $challengeId])
+            ->withData(['notification_id'=> $notificationId,'type'=>$type])
             ->withAndroidConfig(AndroidConfig::fromArray(['ttl' => '3600s']))
             ->withNotification(FcmNotification::create($title, $body)); // Use withNotification() for Android-specific settings
         $messaging->send($message);
