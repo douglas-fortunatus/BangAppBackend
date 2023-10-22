a29f3a11aeaec2c5c3989c356d8fe87622b49c4f
martin kaboja <martinkaboja@gmail.com>
2023-10-22 19:52:29 +0300

    .

--- app/Http/Controllers/PushNotificationService.php
+++ app/Http/Controllers/PushNotificationService.php
@@ -12,13 +12,13 @@ use Kreait\Firebase\Messaging\CloudMessage;
 class PushNotificationService extends Controller
 {
     //
-    public function sendPushNotification($deviceToken,$title, $body,$notificationId,$type)
+    public function sendPushNotification($deviceToken,$title, $body,$notificationId,$type,$userName=null)
     {
         $messaging = app('firebase.messaging');
         // Create a new Message instance with the notification details
         $message = CloudMessage::withTarget('token', $deviceToken)
             ->withNotification(Notification::create($title, $body))
-            ->withData(['notification_id'=> $notificationId,'type'=>$type])
+            ->withData(['notification_id'=> $notificationId,'type'=>$type,'user_name'=>$userName])
             ->withAndroidConfig(AndroidConfig::fromArray(['ttl' => '3600s']))
             ->withNotification(FcmNotification::create($title, $body)); // Use withNotification() for Android-specific settings
         $messaging->send($message);
