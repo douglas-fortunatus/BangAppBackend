1df4b224a7c8bca0c1e3496798ffa606362278ee
hermesgido <hermessgido@gmail.com>
2023-11-19 16:58:32 +0300

    messg

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -133,7 +133,7 @@ class ChatController extends Controller
     $conversation->messages()->save($message);
     $pushNotificationService = new PushNotificationService();
     $pushNotificationService->sendPushNotification($receiver->device_token, $receiver->name, "New Message:" .$messageText, $receiver->id, "message", userName: $sender->name,);
-    saveNotification($sender->id, likeMessage(), 'message', $receiver->id, $conversation->id);
+    saveNotification($sender->id, likeMessage(), 'message', $sender->id, $conversation->id);
 
     // saveNotification($request->user_id, chatMessage($messageText), 'chat', $receiver->id, $conversation->id);
 
