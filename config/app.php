c7550915059725efba15971a10272b8ce37bb7c3
Martin Kaboja <martinkaboja@gmail.com>
2024-01-20 08:33:37 +0000

    .

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/social-backend-laravel'),
+    'url' => env('APP_URL', 'https://bangapp.pro/BangAppBackend/'),
 
-    'asset_url' => env('ASSET_URL', 'http://localhost/social-backend-laravel/public'),
+    'asset_url' => env('ASSET_URL', 'https://bangapp.pro/BangAppBackend/public'),
 
     /*
     |--------------------------------------------------------------------------
